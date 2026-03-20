d2174068c4efd7684db4a9eb9726198e0dc61363
24oanh <oanhoanh2004.vn@gmail.com>
2026-03-20 10:49:43 +0700

    Implement KNN classifier

--- src/models/knn.py
+++ src/models/knn.py
@@ -16,3 +16,37 @@
 #   Siêu tham số:
 #     - k: số láng giềng
 #     - độ đo khoảng cách
+import numpy as np
+
+class KNN_scratch:
+    def __init__(self, k=5, metric='euclidean'):
+        self.k = k
+        self.metric = metric
+        self.X_train = None
+        self.y_train = None
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def _compute_distances(self, X):
+        if self.metric == 'euclidean':
+            X_sq   = np.sum(X**2, axis=1, keepdims=True)
+            Xtr_sq = np.sum(self.X_train**2, axis=1, keepdims=True).T
+            dot    = X @ self.X_train.T
+            dist_sq = np.maximum(X_sq + Xtr_sq - 2*dot, 0)
+            return np.sqrt(dist_sq)
+        elif self.metric == 'manhattan':
+            return np.sum(np.abs(X[:, None, :] - self.X_train[None, :, :]), axis=2)
+        else:
+            raise ValueError(f"Metric '{self.metric}' chưa được hỗ trợ")
+
+    def predict(self, X):
+        distances = self._compute_distances(X)
+        k_nearest_idx = np.argsort(distances, axis=1)[:, :self.k]
+        predictions = []
+        for i in range(len(X)):
+            k_labels = self.y_train[k_nearest_idx[i]]
+            unique, counts = np.unique(k_labels, return_counts=True)
+            predictions.append(unique[np.argmax(counts)])
+        return np.array(predictions)
